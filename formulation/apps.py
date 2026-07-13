890068d80888c799c3f0cc0ae76b8bc47eaa5781
Sheikh Nahida <sheikhnahida140email@gmail.com>
2026-07-13 14:29:29 +0530

    Auto import ingredients on startup

--- formulation/apps.py
+++ formulation/apps.py
@@ -2,5 +2,14 @@ from django.apps import AppConfig
 
 
 class FormulationConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'formulation'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "formulation"
+
+    def ready(self):
+        from django.core.management import call_command
+
+        try:
+            call_command("import_ingredients")
+            call_command("import_nutrient_constraints")
+        except Exception as e:
+            print("Auto import error:", e)
