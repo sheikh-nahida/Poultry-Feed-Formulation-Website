bac189501e51f8b9eada2f1c3f8a1938d4d4f722
Sheikh Nahida <sheikhnahida140email@gmail.com>
2026-07-14 11:08:49 +0530

    Import call_command and HttpResponse

--- formulation/views.py
+++ formulation/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.db.models import Min
+from django.http import HttpResponse
+from django.core.management import call_command
 
 from .models import (
     Ingredient,
