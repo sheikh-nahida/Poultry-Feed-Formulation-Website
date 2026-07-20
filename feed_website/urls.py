beddb2f5872dc6a9f72fc88657f319ea671ddfb9
Sheikh Nahida <sheikhnahida140email@gmail.com>
2026-07-20 10:19:50 +0530

    Make login page the homepage

--- feed_website/urls.py
+++ feed_website/urls.py
@@ -1,19 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-from formulation import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    # Admin
     path('admin/', admin.site.urls),
 
-    # Home page
-    path('', views.index, name='index'),
+    # Login page becomes the home page
+    path('', auth_views.LoginView.as_view(), name='home'),
 
-    # Formulation app
+    # Your app
     path('formulation/', include('formulation.urls')),
 
-    # Authentication (login, logout, password reset, etc.)
+    # Login, Logout, Password Reset
     path('accounts/', include('django.contrib.auth.urls')),
 ]
-
-
